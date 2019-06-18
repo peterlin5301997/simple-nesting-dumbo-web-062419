999876b84958a1b1668535ee8cff24963080d41c
enchanting-process-7328 <peterlin5301997@gmail.com>
2019-06-18 21:20:03 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -56,7 +56,8 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
-return programmer_hash[:dennis_ritchie][:languages].to_s
+string = programmer_hash[:dennis_ritchie][:languages]
+return string
 end
 
 def adding_matz
